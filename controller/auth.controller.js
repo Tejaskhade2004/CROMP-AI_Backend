f83cf23d309255ef4726529baa621a1cdfa5bf86
Tejaskhade2004 <147784743+Tejaskhade2004@users.noreply.github.com>
2026-03-30 15:55:54 +0530

    Update auth.controller.js

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -62,8 +62,8 @@ export const logOut = async (req, res) => {
     try {
         res.clearCookie("token", {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax"
+            secure: true,
+            sameSite: "none"
         })
         return res.status(200).json({
             message: "Logout Success"
